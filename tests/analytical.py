656345d24545ab07688aeec46dd28b81c04e9bcb
Rafael-G-C <61097401+Rafael-G-C@users.noreply.github.com>
2020-11-04 09:50:09 -0600

    differentiating with respect to A and B (#2)

--- tests/analytical.py
+++ tests/analytical.py
@@ -7,11 +7,14 @@ def gradient(x_a, y_a, z_a, x_b, y_b, z_b, c_a, c_b):
     cxc = c_a * c_b
     xyz_squared = ((x_a - x_b) ** 2 + (y_a - y_b) ** 2 + (z_a - z_b) ** 2) ** (3 / 2)
 
-    d_x = -(x_a - x_b) * cxc / xyz_squared
-    d_y = -(y_a - y_b) * cxc / xyz_squared
-    d_z = -(z_a - z_b) * cxc / xyz_squared
+    d_ax = -(x_a - x_b) * cxc / xyz_squared
+    d_ay = -(y_a - y_b) * cxc / xyz_squared
+    d_az = -(z_a - z_b) * cxc / xyz_squared
 
-    return np.array([d_x, d_y, d_z])
+    d_bx = -d_ax
+    d_by = -d_ay
+    d_bz = -d_az
+    return np.array([d_ax, d_ay, d_az, d_bx, d_by, d_bz])
 
 
 def hessian(x_a, y_a, z_a, x_b, y_b, z_b, c_a, c_b):
@@ -20,17 +23,66 @@ def hessian(x_a, y_a, z_a, x_b, y_b, z_b, c_a, c_b):
     cxc = c_a * c_b
     xyz_squared = (x_a - x_b) ** 2 + (y_a - y_b) ** 2 + (z_a - z_b) ** 2
 
-    d_xx = (
+    d_axax = (
         3 * cxc * (x_a - x_b) ** 2 / xyz_squared ** (5 / 2)
     ) - cxc / xyz_squared ** (3 / 2)
-    d_xy = (3 * cxc * (x_a - x_b) * (y_a - y_b)) / xyz_squared ** (5 / 2)
-    d_xz = (3 * cxc * (x_a - x_b) * (z_a - z_b)) / xyz_squared ** (5 / 2)
-    d_yy = (
+
+    d_axbx = cxc / xyz_squared ** (3 / 2) - 3 * cxc * (
+        x_a - x_b
+    ) ** 2 / xyz_squared ** (5 / 2)
+
+    d_axay = (3 * cxc * (x_a - x_b) * (y_a - y_b)) / xyz_squared ** (5 / 2)
+    d_axby = -d_axay
+
+    d_axaz = (3 * cxc * (x_a - x_b) * (z_a - z_b)) / xyz_squared ** (5 / 2)
+    d_axbz = -d_axaz
+
+    d_ayay = (
         3 * cxc * (y_a - y_b) ** 2 / xyz_squared ** (5 / 2)
     ) - cxc / xyz_squared ** (3 / 2)
-    d_yz = (3 * cxc * (y_a - y_b) * (z_a - z_b)) / xyz_squared ** (5 / 2)
-    d_zz = (
+    d_aybx = -d_axay
+    d_ayby = cxc / xyz_squared ** (3 / 2) - (
+        3 * cxc * (y_a - y_b) ** 2 / xyz_squared ** (5 / 2)
+    )
+
+    d_ayaz = (3 * cxc * (y_a - y_b) * (z_a - z_b)) / xyz_squared ** (5 / 2)
+    d_aybz = -d_ayaz
+    d_azaz = (
         3 * cxc * (z_a - z_b) ** 2 / xyz_squared ** (5 / 2)
     ) - cxc / xyz_squared ** (3 / 2)
-
-    return np.array([d_xx, d_xy, d_xz, d_yy, d_yz, d_zz])
+    d_azbx = -d_axaz
+    d_azby = -d_ayaz
+    d_azbz = cxc / xyz_squared ** (3 / 2) - (
+        3 * cxc * (z_a - z_b) ** 2 / xyz_squared ** (5 / 2)
+    )
+    d_bxbx = d_axax
+    d_bxby = d_axay
+    d_bxbz = d_axaz
+    d_byby = d_ayay
+    d_bybz = d_ayaz
+    d_bzbz = d_azaz
+    return np.array(
+        [
+            d_axax,
+            d_axay,
+            d_axaz,
+            d_axbx,
+            d_axby,
+            d_axbz,
+            d_ayay,
+            d_ayaz,
+            d_aybx,
+            d_ayby,
+            d_aybz,
+            d_azaz,
+            d_azbx,
+            d_azby,
+            d_azbz,
+            d_bxbx,
+            d_bxby,
+            d_bxbz,
+            d_byby,
+            d_bybz,
+            d_bzbz,
+        ]
+    )
