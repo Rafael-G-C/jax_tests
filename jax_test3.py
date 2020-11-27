553e1fe1599323d8a531ff12a6eedbdb58b72b9f
Rafael <rafael.g.p.p@outlook.com>
2020-11-26 19:20:43 -0600

    added the ability to work with multiple atoms (coordinates and charges)

--- jax_test3.py
+++ jax_test3.py
@@ -8,34 +8,50 @@ from jax import grad, jacfwd, jacrev
 
 def E_calc(x_a, y_a, z_a, x_b, y_b, z_b, c_a, c_b):
     d = jnp.sqrt((x_a - x_b) ** 2 + (y_a - y_b) ** 2 + (z_a - z_b) ** 2)
-    E = (c_a * c_b) / d
-    return E
+    energy = (c_a * c_b) / d
+    return energy
 
 
-def get_grad(var, x_a, y_a, z_a, x_b, y_b, z_b, c_a, c_b):
+def get_grad(coordinates,charges):
+    number_coordinates = len(coordinates)
     eval_E = []
-    for i in range(var):
-        g_E_calc = grad(E_calc, i)
-        eval_E.append(g_E_calc(x_a, y_a, z_a, x_b, y_b, z_b, c_a, c_b))
+    for atom_a in range(number_coordinates):
+        for atom_b in range(number_coordinates):
+            if atom_a < atom_b:
+                for i in range(6): 
+                    g_E_calc = grad(E_calc, i)
+                    eval_E.append(g_E_calc(coordinates[atom_a,0],coordinates[atom_a,1],coordinates[atom_a,2],coordinates[atom_b,0],coordinates[atom_b,1],coordinates[atom_b,2],charges[atom_a],charges[atom_b]))
     return jnp.array(eval_E)
 
 
-def get_hessian(var, x_a, y_a, z_a, x_b, y_b, z_b, c_a, c_b):
+def get_hessian(coordinates,charges):
     eval_E = []
-    for i in range(var):
-        g_E_calc = grad(E_calc, i)
-        for a in range(var):
-            if i <= a:
-                gg_E_calc = grad(g_E_calc, a)
-                eval_E.append(gg_E_calc(x_a, y_a, z_a, x_b, y_b, z_b, c_a, c_b))
-                # print(f"{i},{a} {eval_E}")
+    number_coordinates = len(coordinates)
+    for atom_a in range(number_coordinates):
+        for atom_b in range(number_coordinates):
+            if atom_a < atom_b:
+
+                for i in range(6):
+                    g_E_calc = grad(E_calc, i)
+                    for a in range(6):
+                        if i <= a:
+                            gg_E_calc = grad(g_E_calc, a)
+                            eval_E.append(gg_E_calc(coordinates[atom_a,0],coordinates[atom_a,1],coordinates[atom_a,2],coordinates[atom_b,0],coordinates[atom_b,1],coordinates[atom_b,2],charges[atom_a],charges[atom_b]))
+                            # print(f"{i},{a} {eval_E}")
     return jnp.array(eval_E)
 
 
-# var = 3
-# max_var_list = jnp.arange(var)
+coordinates = jnp.array([[1.0,2.0,3.0],[2.0,3.0,2.0],[1.0,2.0,4.0]])
+charges = [1.0,1.0,1.0]
+#E_list = E_calc(coordinates,charges)
+#print(E_list) 
+#var = 3
 
 
-# gg_E_calc = grad(g_E_calc)
-# eval_E = gg_E_calc(1.0,2.0,3.0)
-# print(eval_E)
+#gg_E_calc = grad(E_calc, 0)
+#testo = gg_E_calc(1.0,2.0,3.0,1.0,3.0,2.0,1.0,1.0)
+#print(testo)
+
+grad_E = get_hessian(jnp.array([[1.0,2.0,3.0],[2.0,1.0,3.0],[3.0,1.0,2.0]]),[1.0,1.0,1.0])
+print(grad_E)
+#print(eval_E)
--- tests/analytical.py
+++ tests/analytical.py
@@ -1,88 +1,111 @@
 import numpy as np
 
 
-def gradient(x_a, y_a, z_a, x_b, y_b, z_b, c_a, c_b):
+def gradient(coordinates,charges):
     """Computes the gradient."""
+    number_coordinates = len(coordinates)
+    eval_E = []
+    for atom_a in range(number_coordinates):
+        for atom_b in range(number_coordinates):
+            if atom_a < atom_b:
+                cxc = charges[atom_a] * charges[atom_b]
+                xyz_squared = ((coordinates[atom_a,0] - coordinates[atom_b,0]) ** 2 + (coordinates[atom_a,1] - coordinates[atom_b,1]) ** 2 + (coordinates[atom_a,2] - coordinates[atom_b,2]) ** 2) ** (3 / 2)
 
-    cxc = c_a * c_b
-    xyz_squared = ((x_a - x_b) ** 2 + (y_a - y_b) ** 2 + (z_a - z_b) ** 2) ** (3 / 2)
+                d_ax = -(coordinates[atom_a,0] - coordinates[atom_b,0]) * cxc / xyz_squared
+                eval_E.append(d_ax)
+                d_ay = -(coordinates[atom_a,1] - coordinates[atom_b,1]) * cxc / xyz_squared
+                eval_E.append(d_ay)
+                d_az = -(coordinates[atom_a,2] - coordinates[atom_b,2]) * cxc / xyz_squared
+                eval_E.append(d_az)
 
-    d_ax = -(x_a - x_b) * cxc / xyz_squared
-    d_ay = -(y_a - y_b) * cxc / xyz_squared
-    d_az = -(z_a - z_b) * cxc / xyz_squared
+                d_bx = -d_ax
+                eval_E.append(d_bx)
+                d_by = -d_ay
+                eval_E.append(d_by)
+                d_bz = -d_az
+                eval_E.append(d_bz)
 
-    d_bx = -d_ax
-    d_by = -d_ay
-    d_bz = -d_az
-    return np.array([d_ax, d_ay, d_az, d_bx, d_by, d_bz])
+    return np.array(eval_E)
 
 
-def hessian(x_a, y_a, z_a, x_b, y_b, z_b, c_a, c_b):
+def hessian(coordinates,charges):
     """Computes the Hessian."""
+    number_coordinates = len(coordinates)
+    eval_E = []
+    for atom_a in range(number_coordinates):
+        for atom_b in range(number_coordinates):
+            if atom_a < atom_b:
+                cxc = charges[atom_a] * charges[atom_b]
+                x_a = coordinates[atom_a,0]
+                y_a = coordinates[atom_a,1]
+                z_a = coordinates[atom_a,2]
+                x_b = coordinates[atom_b,0]
+                y_b = coordinates[atom_b,1]
+                z_b = coordinates[atom_b,2]
 
-    cxc = c_a * c_b
-    xyz_squared = (x_a - x_b) ** 2 + (y_a - y_b) ** 2 + (z_a - z_b) ** 2
+                xyz_squared = (x_a - x_b) ** 2 + (y_a - y_b) ** 2 + (z_a - z_b) ** 2
 
-    d_axax = (
-        3 * cxc * (x_a - x_b) ** 2 / xyz_squared ** (5 / 2)
-    ) - cxc / xyz_squared ** (3 / 2)
+                d_axax = (
+                    3 * cxc * (x_a - x_b) ** 2 / xyz_squared ** (5 / 2)
+                ) - cxc / xyz_squared ** (3 / 2)
 
-    d_axbx = cxc / xyz_squared ** (3 / 2) - 3 * cxc * (
-        x_a - x_b
-    ) ** 2 / xyz_squared ** (5 / 2)
+                d_axbx = cxc / xyz_squared ** (3 / 2) - 3 * cxc * (
+                    x_a - x_b
+                ) ** 2 / xyz_squared ** (5 / 2)
 
-    d_axay = (3 * cxc * (x_a - x_b) * (y_a - y_b)) / xyz_squared ** (5 / 2)
-    d_axby = -d_axay
+                d_axay = (3 * cxc * (x_a - x_b) * (y_a - y_b)) / xyz_squared ** (5 / 2)
+                d_axby = -d_axay
 
-    d_axaz = (3 * cxc * (x_a - x_b) * (z_a - z_b)) / xyz_squared ** (5 / 2)
-    d_axbz = -d_axaz
+                d_axaz = (3 * cxc * (x_a - x_b) * (z_a - z_b)) / xyz_squared ** (5 / 2)
+                d_axbz = -d_axaz
 
-    d_ayay = (
-        3 * cxc * (y_a - y_b) ** 2 / xyz_squared ** (5 / 2)
-    ) - cxc / xyz_squared ** (3 / 2)
-    d_aybx = -d_axay
-    d_ayby = cxc / xyz_squared ** (3 / 2) - (
-        3 * cxc * (y_a - y_b) ** 2 / xyz_squared ** (5 / 2)
-    )
+                d_ayay = (
+                    3 * cxc * (y_a - y_b) ** 2 / xyz_squared ** (5 / 2)
+                ) - cxc / xyz_squared ** (3 / 2)
+                d_aybx = -d_axay
+                d_ayby = cxc / xyz_squared ** (3 / 2) - (
+                    3 * cxc * (y_a - y_b) ** 2 / xyz_squared ** (5 / 2)
+                )
 
-    d_ayaz = (3 * cxc * (y_a - y_b) * (z_a - z_b)) / xyz_squared ** (5 / 2)
-    d_aybz = -d_ayaz
-    d_azaz = (
-        3 * cxc * (z_a - z_b) ** 2 / xyz_squared ** (5 / 2)
-    ) - cxc / xyz_squared ** (3 / 2)
-    d_azbx = -d_axaz
-    d_azby = -d_ayaz
-    d_azbz = cxc / xyz_squared ** (3 / 2) - (
-        3 * cxc * (z_a - z_b) ** 2 / xyz_squared ** (5 / 2)
-    )
-    d_bxbx = d_axax
-    d_bxby = d_axay
-    d_bxbz = d_axaz
-    d_byby = d_ayay
-    d_bybz = d_ayaz
-    d_bzbz = d_azaz
-    return np.array(
-        [
-            d_axax,
-            d_axay,
-            d_axaz,
-            d_axbx,
-            d_axby,
-            d_axbz,
-            d_ayay,
-            d_ayaz,
-            d_aybx,
-            d_ayby,
-            d_aybz,
-            d_azaz,
-            d_azbx,
-            d_azby,
-            d_azbz,
-            d_bxbx,
-            d_bxby,
-            d_bxbz,
-            d_byby,
-            d_bybz,
-            d_bzbz,
-        ]
-    )
+                d_ayaz = (3 * cxc * (y_a - y_b) * (z_a - z_b)) / xyz_squared ** (5 / 2)
+                d_aybz = -d_ayaz
+                d_azaz = (
+                    3 * cxc * (z_a - z_b) ** 2 / xyz_squared ** (5 / 2)
+                ) - cxc / xyz_squared ** (3 / 2)
+                d_azbx = -d_axaz
+                d_azby = -d_ayaz
+                d_azbz = cxc / xyz_squared ** (3 / 2) - (
+                    3 * cxc * (z_a - z_b) ** 2 / xyz_squared ** (5 / 2)
+                )
+                d_bxbx = d_axax
+                d_bxby = d_axay
+                d_bxbz = d_axaz
+                d_byby = d_ayay
+                d_bybz = d_ayaz
+                d_bzbz = d_azaz
+
+                eval_E.append(d_axax)
+                eval_E.append(d_axay)
+                eval_E.append(d_axaz)
+                eval_E.append(d_axbx)
+                eval_E.append(d_axby)
+                eval_E.append(d_axbz)
+                eval_E.append(d_ayay)
+                eval_E.append(d_ayaz)
+                eval_E.append(d_aybx)
+                eval_E.append(d_ayby)
+                eval_E.append(d_aybz)
+                eval_E.append(d_azaz)
+                eval_E.append(d_azbx)
+                eval_E.append(d_azby)
+                eval_E.append(d_azbz)
+                eval_E.append(d_bxbx)
+                eval_E.append(d_bxby)
+                eval_E.append(d_bxbz)
+                eval_E.append(d_byby)
+                eval_E.append(d_bybz)
+                eval_E.append(d_bzbz)
+
+    return np.array(eval_E)
+a_gradient = hessian(np.array([[1.0,2.0,3.0],[2.0,1.0,3.0],[3.0,1.0,2.0]]),[1.0,1.0,1.0])
+print(a_gradient)
