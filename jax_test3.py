right logic wrong result

jax_test3.py
@@ -1,37 +1,100 @@
 from jax.config import config
+import numpy as np
 
 config.update("jax_enable_x64", True)
 
 import jax.numpy as jnp
 from jax import grad, jacfwd, jacrev
 
+def _derv_sequence(orders):
+    sequence = []
+    for variable, variable_order in enumerate(orders):
+        if variable_order > 0:
+            sequence += variable_order * [variable]
+    return sequence
 
-def E_calc(coordinates, charges):
-    energy = 0.0
-    natoms = charges.size
-    for A in range(natoms):
+
+def test_derv_sequence():
+    assert _derv_sequence((3, 2, 1, 0)) == [0, 0, 0, 1, 1, 2]
+    assert _derv_sequence((0, 1, 2, 3)) == [1, 2, 2, 3, 3, 3]
+    assert _derv_sequence((0, 1, 0, 1)) == [1, 3]
+
+
+def derv(fun, variables, orders) -> float:
+    """
+    fun: function to differentiate which expects a certain number of variables
+    variables: list of variables at which to differentiate the function
+    orders: [1, 0, 2, 0] means differentate with respect to variable 1 once,
+                         and differentiate with respect to variable 3 twice.
+    """
+    sequence = _derv_sequence(orders)
+    functions = [fun]
+    for i, order in enumerate(sequence):
+        functions.append(grad(functions[i], (order)))
+    return functions[-1](*variables)
+
+def some_data(charges):
+    natoms = len(charges)
+    state = np.zeros(2+3*natoms, dtype=int)
+    return natoms, state
+
+def E_NN(N, Zs, *Rs):  
+    if len(Rs) != N * 3:
+        raise RuntimeError(f"Must have {3*N} coordinates, got {len(Rs)}") 
+
+    if len(Zs) != N:
+        raise RuntimeError(f"Must have {N} charges, got {len(Zs)}")
+    
+
+    e_nn = 0
+    for A in range(N):
         for B in range(A):
-            d = jnp.linalg.norm(coordinates[A, :] - coordinates[B, :])
-            energy += charges[A] * charges[B] / d
-    return energy
+            d_AB = jnp.sqrt((Rs[3*A] - Rs[3*B])**2 + (Rs[3*A+1] - Rs[3*B+1])**2 + (Rs[3*A+2] - Rs[3*B+2])**2)
+            e_nn += Zs[A] * Zs[B] / d_AB
+    
+    return e_nn
 
 
-def get_grad(coordinates, charges):
-    g_E_calc = grad(E_calc)
-    eval_E = g_E_calc(coordinates, charges)
-    return eval_E
+def get_grad(coordinates,charges):
+    gradient = []
+    natoms, state = some_data(charges)
+    for item in range(2,len(state)):
+        state[item] = 1
+        eval_E = derv(E_NN, [charges.size, charges, *(coordinates.reshape(3*natoms).tolist())], state)
+        gradient.append(eval_E)
+        state[item] = 0
+
+    gradient = np.array(gradient)
+    gradient = gradient.reshape((natoms,3))
+    return gradient
 
 
-def get_hessian(coordinates, charges):
-    gg_E_calc = jacfwd(jacrev(E_calc))
-    eval_E = gg_E_calc(coordinates, charges)
-    return eval_E
+def get_hessian(coordinates,charges):
+    natoms, state = some_data(charges)
+    hessian = np.zeros((natoms, 3) * 2)
+    print(hessian.shape)
+    i_xyz = 0
+    a_xyz = 0
+    for i in range(2,len(state)):
+        state[i] = 1
+        for a in range(2,len(state)):
+            state[a] = 1     
+            eval_E = derv(E_NN, [charges.size, charges, *(coordinates.reshape(3*natoms).tolist())], state)
+            print(f"mapping to {(i-2)//3},{i_xyz},{(a-2)//3},{a_xyz}")
+            hessian[(i-2)//3,i_xyz,(a-2)//3,a_xyz] = eval_E
+            state[a] = 0
+            a_xyz += 1
+            a_xyz *= a_xyz != 3
+                
+        state[i] = 0
+        i_xyz += 1
+        i_xyz *= i_xyz != 3 
 
+    return hessian
 
+coordinates = np.array([[1.0, 2.0, 3.0], [2.0, 1.0, 3.0]])
+charges = np.array([1.0, 1.0])
 """
-coordinates = jnp.array([[2.0,3.0,2.0],[1.0,2.0,3.0]])
-charges = jnp.array([1.0,1.0])
-
 #E_list = E_calc(coordinates,charges)
 #print(E_list) 
 
@@ -41,7 +104,7 @@ print(test)
 gg_E_calc = grad(g_E_calc,0)
 testo = gg_E_calc(coordinates,charges)
 print(testo)
-
-#grad_E = get_hessian(coordinates,charges)
-#print(grad_E)
 """
+grad_E = get_hessian(coordinates,charges)
+print(grad_E)
+

tests/analytical.py
@@ -101,75 +101,8 @@ def hessian_redundant(coordinates, charges):
     return gg
 
 
-def hessian(coordinates, charges):
-    """Computes the Hessian."""
-    natoms = charges.size
-    eval_E = []
-    for A in range(charges.size):
-        for B in range(charges.size):
-            if A < B:
-                cxc = charges[A] * charges[B]
-                x_a = coordinates[A, 0]
-                y_a = coordinates[A, 1]
-                z_a = coordinates[A, 2]
-                x_b = coordinates[B, 0]
-                y_b = coordinates[B, 1]
-                z_b = coordinates[B, 2]
+coordinates = np.array([[1.0, 2.0, 3.0], [2.0, 1.0, 3.0]])
+charges = np.array([1.0, 1.0])
 
-                # distance between A and B
-                dist = np.linalg.norm(coordinates[A, :] - coordinates[B, :])
-                # third power of distance between A and B
-                dist_32 = np.linalg.norm(coordinates[A, :] - coordinates[B, :]) ** 3
-                # fifth power of distance between A and B
-                dist_52 = np.linalg.norm(coordinates[A, :] - coordinates[B, :]) ** 5
-
-                d_axax = (3 * cxc * (x_a - x_b) ** 2 / dist_52) - cxc / dist_32
-
-                d_axbx = cxc / dist_32 - 3 * cxc * (x_a - x_b) ** 2 / dist_52
-
-                d_axay = (3 * cxc * (x_a - x_b) * (y_a - y_b)) / dist_52
-                d_axby = -d_axay
-
-                d_axaz = (3 * cxc * (x_a - x_b) * (z_a - z_b)) / dist_52
-                d_axbz = -d_axaz
-
-                d_ayay = (3 * cxc * (y_a - y_b) ** 2 / dist_52) - cxc / dist_32
-                d_aybx = -d_axay
-                d_ayby = cxc / dist_32 - (3 * cxc * (y_a - y_b) ** 2 / dist_52)
-
-                d_ayaz = (3 * cxc * (y_a - y_b) * (z_a - z_b)) / dist_52
-                d_aybz = -d_ayaz
-                d_azaz = (3 * cxc * (z_a - z_b) ** 2 / dist_52) - cxc / dist_32
-                d_azbx = -d_axaz
-                d_azby = -d_ayaz
-                d_azbz = cxc / dist_32 - (3 * cxc * (z_a - z_b) ** 2 / dist_52)
-                d_bxbx = d_axax
-                d_bxby = d_axay
-                d_bxbz = d_axaz
-                d_byby = d_ayay
-                d_bybz = d_ayaz
-                d_bzbz = d_azaz
-
-                eval_E.append(d_axax)
-                eval_E.append(d_axay)
-                eval_E.append(d_axaz)
-                eval_E.append(d_axbx)
-                eval_E.append(d_axby)
-                eval_E.append(d_axbz)
-                eval_E.append(d_ayay)
-                eval_E.append(d_ayaz)
-                eval_E.append(d_aybx)
-                eval_E.append(d_ayby)
-                eval_E.append(d_aybz)
-                eval_E.append(d_azaz)
-                eval_E.append(d_azbx)
-                eval_E.append(d_azby)
-                eval_E.append(d_azbz)
-                eval_E.append(d_bxbx)
-                eval_E.append(d_bxby)
-                eval_E.append(d_bxbz)
-                eval_E.append(d_byby)
-                eval_E.append(d_bybz)
-                eval_E.append(d_bzbz)
-
-    return np.array(eval_E)
+a_hessian = hessian_redundant(coordinates,charges)
+print(a_hessian)