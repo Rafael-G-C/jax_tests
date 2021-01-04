3c9254731e80036fe9e07dc45b92bdc119b41680
Rafael-G-C <61097401+Rafael-G-C@users.noreply.github.com>
2021-01-04 03:41:51 -0600

    changed the gradient input and corresponding grad (#9)
    * changed the gradient input and corresponding grad
    
    * Format code using Black
    
    * Remove print out
    
    * Use non-unit charges in tests
    
    * working analytical hessian, jax gradient and hessian
    
    * Flatten coordinates array outside the loop
    
    * Use less verbose form of variadic argument-passing
    
    Co-authored-by: Roberto Di Remigio <roberto.diremigio@gmail.com>

--- tests/analytical.py
+++ tests/analytical.py
@@ -1,113 +1,118 @@
 import numpy as np
 
 
-def gradient(coordinates,charges):
+def gradient(coordinates, charges):
     """Computes the gradient."""
-    number_coordinates = len(coordinates)
-    eval_E = []
-    for atom_a in range(number_coordinates):
-        for atom_b in range(number_coordinates):
-            if atom_a < atom_b:
-                cxc = charges[atom_a] * charges[atom_b]
-                xyz_squared = ((coordinates[atom_a,0] - coordinates[atom_b,0]) ** 2 + (coordinates[atom_a,1] - coordinates[atom_b,1]) ** 2 + (coordinates[atom_a,2] - coordinates[atom_b,2]) ** 2) ** (3 / 2)
+    g = np.zeros_like(coordinates)
 
-                d_ax = -(coordinates[atom_a,0] - coordinates[atom_b,0]) * cxc / xyz_squared
-                eval_E.append(d_ax)
-                d_ay = -(coordinates[atom_a,1] - coordinates[atom_b,1]) * cxc / xyz_squared
-                eval_E.append(d_ay)
-                d_az = -(coordinates[atom_a,2] - coordinates[atom_b,2]) * cxc / xyz_squared
-                eval_E.append(d_az)
+    for A in range(charges.size):
+        for B in range(charges.size):
+            if A < B:
+                cxc = charges[A] * charges[B]
+                xyz_32 = np.linalg.norm(coordinates[A, :] - coordinates[B, :]) ** 3
+                g[A, :] -= (coordinates[A, :] - coordinates[B, :]) * cxc / xyz_32
+                g[B, :] -= g[A, :]
+
+    return g
 
 
-                d_bx = -d_ax
-                eval_E.append(d_bx)
-                d_by = -d_ay
-                eval_E.append(d_by)
-                d_bz = -d_az
-                eval_E.append(d_bz)
+def hessian_redundant(coordinates, charges):
+    """Computes the Hessian in redundant format
 
-    return np.array(eval_E)
+    Compute [3N*3N] second derivatives, rather than justs [3N*(3N+1)/2].
+    """
+    natoms = charges.size
+    gg = np.zeros((natoms, 3) * 2)
+    for A in range(charges.size):
+        for B in range(charges.size):
+            if A < B:
+                cxc = charges[A] * charges[B]
+                x_a = coordinates[A, 0]
+                y_a = coordinates[A, 1]
+                z_a = coordinates[A, 2]
+                x_b = coordinates[B, 0]
+                y_b = coordinates[B, 1]
+                z_b = coordinates[B, 2]
+
+                # distance between A and B
+                dist = np.linalg.norm(coordinates[A, :] - coordinates[B, :])
+                # third power of distance between A and B
+                dist_32 = np.linalg.norm(coordinates[A, :] - coordinates[B, :]) ** 3
+                # fifth power of distance between A and B
+                dist_52 = np.linalg.norm(coordinates[A, :] - coordinates[B, :]) ** 5
+
+                # AxAx
+                gg[A, 0, A, 0] += (3 * cxc * (x_a - x_b) ** 2 / dist_52) - cxc / dist_32
+                # AxAy and AyAx
+                gg[A, 0, A, 1] += (3 * cxc * (x_a - x_b) * (y_a - y_b)) / dist_52
+                gg[A, 1, A, 0] = gg[A, 0, A, 1]
+                # AxAz and AzAx
+                gg[A, 0, A, 2] += (3 * cxc * (x_a - x_b) * (z_a - z_b)) / dist_52
+                gg[A, 2, A, 0] = gg[A, 0, A, 2]
+                # AyAy
+                gg[A, 1, A, 1] += (3 * cxc * (y_a - y_b) ** 2 / dist_52) - cxc / dist_32
+                # AyAz and AzAy
+                gg[A, 1, A, 2] += (3 * cxc * (y_a - y_b) * (z_a - z_b)) / dist_52
+                gg[A, 2, A, 1] = gg[A, 1, A, 2]
+                # AzAz
+                gg[A, 2, A, 2] += (3 * cxc * (z_a - z_b) ** 2 / dist_52) - cxc / dist_32
+
+                # AxBx and BxAx
+                gg[A, 0, B, 0] += cxc / dist_32 - 3 * cxc * (x_a - x_b) ** 2 / dist_52
+                gg[B, 0, A, 0] = gg[A, 0, B, 0]
+                # AxBy, AyBx, ByAx, BxAy
+                gg[A, 0, B, 1] += -(3 * cxc * (x_a - x_b) * (y_a - y_b)) / dist_52
+                gg[A, 1, B, 0] = gg[A, 0, B, 1]
+                gg[B, 1, A, 0] = gg[A, 0, B, 1]
+                gg[B, 0, A, 1] = gg[A, 0, B, 1]
+                # AxBz, AzBx, BzAx, BxAz
+                gg[A, 0, B, 2] += -(3 * cxc * (x_a - x_b) * (z_a - z_b)) / dist_52
+                gg[A, 2, B, 0] = gg[A, 0, B, 2]
+                gg[B, 2, A, 0] = gg[A, 0, B, 2]
+                gg[B, 0, A, 2] = gg[A, 0, B, 2]
+                # AyBy and ByAy
+                gg[A, 1, B, 1] += cxc / dist_32 - (3 * cxc * (y_a - y_b) ** 2 / dist_52)
+                gg[B, 1, A, 1] = gg[A, 1, B, 1]
+                # AyBz, AzBy, ByAz, BzAy
+                gg[A, 1, B, 2] += -(3 * cxc * (y_a - y_b) * (z_a - z_b)) / dist_52
+                gg[A, 2, B, 1] = gg[A, 1, B, 2]
+                gg[B, 1, A, 2] = gg[A, 1, B, 2]
+                gg[B, 2, A, 1] = gg[A, 1, B, 2]
+                # AzBz and BzAz
+                gg[A, 2, B, 2] += cxc / dist_32 - (3 * cxc * (z_a - z_b) ** 2 / dist_52)
+                gg[B, 2, A, 2] = gg[A, 2, B, 2]
+
+                # BxBx
+                gg[B, 0, B, 0] += (3 * cxc * (x_a - x_b) ** 2 / dist_52) - cxc / dist_32
+                # BxBy and ByBx
+                gg[B, 0, B, 1] += (3 * cxc * (x_a - x_b) * (y_a - y_b)) / dist_52
+                gg[B, 1, B, 0] = gg[B, 0, B, 1]
+                # BxBz and BzBx
+                gg[B, 0, B, 2] += (3 * cxc * (x_a - x_b) * (z_a - z_b)) / dist_52
+                gg[B, 2, B, 0] = gg[B, 0, B, 2]
+                # ByBy
+                gg[B, 1, B, 1] += (3 * cxc * (y_a - y_b) ** 2 / dist_52) - cxc / dist_32
+                # ByBz and BzBy
+                gg[B, 1, B, 2] += (3 * cxc * (y_a - y_b) * (z_a - z_b)) / dist_52
+                gg[B, 2, B, 1] = gg[B, 1, B, 2]
+                # BzBz
+                gg[B, 2, B, 2] += (3 * cxc * (z_a - z_b) ** 2 / dist_52) - cxc / dist_32
+
+    return gg
 
 
-def hessian(coordinates,charges):
-    """Computes the Hessian."""
-    number_coordinates = len(coordinates)
-    eval_E = []
-    for atom_a in range(number_coordinates):
-        for atom_b in range(number_coordinates):
-            if atom_a < atom_b:
-                cxc = charges[atom_a] * charges[atom_b]
-                x_a = coordinates[atom_a,0]
-                y_a = coordinates[atom_a,1]
-                z_a = coordinates[atom_a,2]
-                x_b = coordinates[atom_b,0]
-                y_b = coordinates[atom_b,1]
-                z_b = coordinates[atom_b,2]
+"""
+coordinates = np.array(
+                [
+                    [1.0, 2.0, 3.0],
+                    [2.0, 1.0, 3.0],
+                    [3.0, 1.0, 2.0],
+                    [5.0, 4.0, 3.0],
+                    [4.0, 3.0, 5.0],
+                ]
+            )
+charges = np.array([7.0, 6.0, 1.0, 5.0, 5.0])
 
-
-                xyz_squared = (x_a - x_b) ** 2 + (y_a - y_b) ** 2 + (z_a - z_b) ** 2
-
-                d_axax = (
-                    3 * cxc * (x_a - x_b) ** 2 / xyz_squared ** (5 / 2)
-                ) - cxc / xyz_squared ** (3 / 2)
-
-                d_axbx = cxc / xyz_squared ** (3 / 2) - 3 * cxc * (
-                    x_a - x_b
-                ) ** 2 / xyz_squared ** (5 / 2)
-
-                d_axay = (3 * cxc * (x_a - x_b) * (y_a - y_b)) / xyz_squared ** (5 / 2)
-                d_axby = -d_axay
-
-                d_axaz = (3 * cxc * (x_a - x_b) * (z_a - z_b)) / xyz_squared ** (5 / 2)
-                d_axbz = -d_axaz
-
-                d_ayay = (
-                    3 * cxc * (y_a - y_b) ** 2 / xyz_squared ** (5 / 2)
-                ) - cxc / xyz_squared ** (3 / 2)
-                d_aybx = -d_axay
-                d_ayby = cxc / xyz_squared ** (3 / 2) - (
-                    3 * cxc * (y_a - y_b) ** 2 / xyz_squared ** (5 / 2)
-                )
-
-                d_ayaz = (3 * cxc * (y_a - y_b) * (z_a - z_b)) / xyz_squared ** (5 / 2)
-                d_aybz = -d_ayaz
-                d_azaz = (
-                    3 * cxc * (z_a - z_b) ** 2 / xyz_squared ** (5 / 2)
-                ) - cxc / xyz_squared ** (3 / 2)
-                d_azbx = -d_axaz
-                d_azby = -d_ayaz
-                d_azbz = cxc / xyz_squared ** (3 / 2) - (
-                    3 * cxc * (z_a - z_b) ** 2 / xyz_squared ** (5 / 2)
-                )
-                d_bxbx = d_axax
-                d_bxby = d_axay
-                d_bxbz = d_axaz
-                d_byby = d_ayay
-                d_bybz = d_ayaz
-                d_bzbz = d_azaz
-
-                eval_E.append(d_axax)
-                eval_E.append(d_axay)
-                eval_E.append(d_axaz)
-                eval_E.append(d_axbx)
-                eval_E.append(d_axby)
-                eval_E.append(d_axbz)
-                eval_E.append(d_ayay)
-                eval_E.append(d_ayaz)
-                eval_E.append(d_aybx)
-                eval_E.append(d_ayby)
-                eval_E.append(d_aybz)
-                eval_E.append(d_azaz)
-                eval_E.append(d_azbx)
-                eval_E.append(d_azby)
-                eval_E.append(d_azbz)
-                eval_E.append(d_bxbx)
-                eval_E.append(d_bxby)
-                eval_E.append(d_bxbz)
-                eval_E.append(d_byby)
-                eval_E.append(d_bybz)
-                eval_E.append(d_bzbz)
-
-    return np.array(eval_E)
-a_gradient = hessian(np.array([[1.0,2.0,3.0],[2.0,1.0,3.0],[3.0,1.0,2.0]]),[1.0,1.0,1.0])
-print(a_gradient)
+a_hessian = hessian_redundant(coordinates,charges)
+#print(a_hessian)
+"""
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -3,33 +3,75 @@ import pytest as pt
 
 import analytical
 import numpy as np
+
+np.set_printoptions(linewidth=120)
+
+
 @pt.mark.parametrize(
     "coordinates, charges",
     [
-        (np.array([[1.0,2.0,3.0],[2.0,1.0,3.0]]),[1.0,1.0]),
-        (np.array([[1.0,2.0,3.0],[2.0,1.0,3.0],[3.0,1.0,2.0]]),[1.0,1.0,1.0]),
-        (np.array([[1.0,2.0,3.0],[2.0,1.0,3.0],[3.0,1.0,2.0],[5.0,4.0,3.0]]),[1.0,1.0,1.0,1.0]),
-        (np.array([[1.0,2.0,3.0],[2.0,1.0,3.0],[3.0,1.0,2.0],[5.0,4.0,3.0],[4.0,3.0,5.0]]),[1.0,1.0,1.0,1.0,1.0])
+        (np.array([[1.0, 2.0, 3.0], [2.0, 1.0, 3.0]]), np.array([1.0, 2.0])),
+        (
+            np.array([[1.0, 2.0, 3.0], [2.0, 1.0, 3.0], [3.0, 1.0, 2.0]]),
+            np.array([2.0, 1.0, 3.0]),
+        ),
+        (
+            np.array(
+                [[1.0, 2.0, 3.0], [2.0, 1.0, 3.0], [3.0, 1.0, 2.0], [5.0, 4.0, 3.0]]
+            ),
+            np.array([3.0, 4.0, 1.0, 7.0]),
+        ),
+        (
+            np.array(
+                [
+                    [1.0, 2.0, 3.0],
+                    [2.0, 1.0, 3.0],
+                    [3.0, 1.0, 2.0],
+                    [5.0, 4.0, 3.0],
+                    [4.0, 3.0, 5.0],
+                ]
+            ),
+            np.array([7.0, 6.0, 1.0, 5.0, 5.0]),
+        ),
     ],
 )
-
-def test_grad(coordinates,charges):
-    ref_grad = analytical.gradient(coordinates,charges)
-    jax_grad = jt3.get_grad(coordinates,charges)
+def test_grad(coordinates, charges):
+    ref_grad = analytical.gradient(coordinates, charges)
+    jax_grad = jt3.get_grad(coordinates, charges)
     assert jax_grad == pt.approx(ref_grad)
 
 
 @pt.mark.parametrize(
     "coordinates, charges",
     [
-        (np.array([[1.0,2.0,3.0],[2.0,1.0,3.0]]),[1.0,1.0]),
-        (np.array([[1.0,2.0,3.0],[2.0,1.0,3.0],[3.0,1.0,2.0]]),[1.0,1.0,1.0]),
-        (np.array([[1.0,2.0,3.0],[2.0,1.0,3.0],[3.0,1.0,2.0],[5.0,4.0,3.0]]),[1.0,1.0,1.0,1.0]),
-        (np.array([[1.0,2.0,3.0],[2.0,1.0,3.0],[3.0,1.0,2.0],[5.0,4.0,3.0],[4.0,3.0,5.0]]),[1.0,1.0,1.0,1.0,1.0])
+        (np.array([[1.0, 2.0, 3.0], [2.0, 1.0, 3.0]]), np.array([1.0, 2.0])),
+        (
+            np.array([[1.0, 2.0, 3.0], [2.0, 1.0, 3.0], [3.0, 1.0, 2.0]]),
+            np.array([2.0, 1.0, 3.0]),
+        ),
+        (
+            np.array(
+                [[1.0, 2.0, 3.0], [2.0, 1.0, 3.0], [3.0, 1.0, 2.0], [5.0, 4.0, 3.0]]
+            ),
+            np.array([3.0, 4.0, 1.0, 7.0]),
+        ),
+        (
+            np.array(
+                [
+                    [1.0, 2.0, 3.0],
+                    [2.0, 1.0, 3.0],
+                    [3.0, 1.0, 2.0],
+                    [5.0, 4.0, 3.0],
+                    [4.0, 3.0, 5.0],
+                ]
+            ),
+            np.array([7.0, 6.0, 1.0, 5.0, 5.0]),
+        ),
     ],
 )
-
-def test_hessian(coordinates,charges):
-    ref_hess = analytical.hessian(coordinates,charges)
-    jax_hess = jt3.get_hessian(coordinates,charges)
+def test_hessian(coordinates, charges):
+    ref_hess = analytical.hessian_redundant(coordinates, charges)
+    print(f"ref_hess\n{ref_hess}")
+    jax_hess = jt3.get_hessian(coordinates, charges)
+    print(f"jax_hess\n{jax_hess}")
     assert jax_hess == pt.approx(ref_hess)
