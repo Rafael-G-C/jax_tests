e99ab6b2e9f34d2ff26ceae68af4401b21133dde
Rafael-G-C <61097401+Rafael-G-C@users.noreply.github.com>
2021-01-04 03:46:49 -0600

    Dealing with multiple atoms (coordinates/charges) (#8)
    * differentiating with respect to A and B
    
    * added the ability to work with multiple atoms (coordinates and charges)
    
    * changed the gradient input and corresponding grad (#9)
    
    * changed the gradient input and corresponding grad
    
    * Format code using Black
    
    * Remove print out
    
    * Use non-unit charges in tests
    
    * working analytical hessian, jax gradient and hessian
    
    * Flatten coordinates array outside the loop
    
    * Use less verbose form of variadic argument-passing
    
    Co-authored-by: Roberto Di Remigio <roberto.diremigio@gmail.com>

--- jax_test3.py
+++ jax_test3.py
@@ -1,4 +1,5 @@
 from jax.config import config
+import numpy as np
 
 config.update("jax_enable_x64", True)
 
@@ -6,36 +7,113 @@ import jax.numpy as jnp
 from jax import grad, jacfwd, jacrev
 
 
-def E_calc(x_a, y_a, z_a, x_b, y_b, z_b, c_a, c_b):
-    d = jnp.sqrt((x_a - x_b) ** 2 + (y_a - y_b) ** 2 + (z_a - z_b) ** 2)
-    E = (c_a * c_b) / d
-    return E
+def _derv_sequence(orders):
+    sequence = []
+    for variable, variable_order in enumerate(orders):
+        if variable_order > 0:
+            sequence += variable_order * [variable]
+    return sequence
 
 
-def get_grad(var, x_a, y_a, z_a, x_b, y_b, z_b, c_a, c_b):
-    eval_E = []
-    for i in range(var):
-        g_E_calc = grad(E_calc, i)
-        eval_E.append(g_E_calc(x_a, y_a, z_a, x_b, y_b, z_b, c_a, c_b))
-    return jnp.array(eval_E)
+def test_derv_sequence():
+    assert _derv_sequence((3, 2, 1, 0)) == [0, 0, 0, 1, 1, 2]
+    assert _derv_sequence((0, 1, 2, 3)) == [1, 2, 2, 3, 3, 3]
+    assert _derv_sequence((0, 1, 0, 1)) == [1, 3]
 
 
-def get_hessian(var, x_a, y_a, z_a, x_b, y_b, z_b, c_a, c_b):
-    eval_E = []
-    for i in range(var):
-        g_E_calc = grad(E_calc, i)
-        for a in range(var):
-            if i <= a:
-                gg_E_calc = grad(g_E_calc, a)
-                eval_E.append(gg_E_calc(x_a, y_a, z_a, x_b, y_b, z_b, c_a, c_b))
-                # print(f"{i},{a} {eval_E}")
-    return jnp.array(eval_E)
+def derv(fun, variables, orders) -> float:
+    """
+    fun: function to differentiate which expects a certain number of variables
+    variables: list of variables at which to differentiate the function
+    orders: [1, 0, 2, 0] means differentate with respect to variable 1 once,
+                         and differentiate with respect to variable 3 twice.
+    """
+    sequence = _derv_sequence(orders)
+    functions = [fun]
+    for i, order in enumerate(sequence):
+        functions.append(grad(functions[i], (order)))
+    return functions[-1](*variables)
 
 
-# var = 3
-# max_var_list = jnp.arange(var)
+def E_NN(N, Zs, *Rs):
+    if len(Rs) != N * 3:
+        raise RuntimeError(f"Must have {3*N} coordinates, got {len(Rs)}")
+
+    if len(Zs) != N:
+        raise RuntimeError(f"Must have {N} charges, got {len(Zs)}")
+
+    e_nn = 0
+    for A in range(N):
+        for B in range(A):
+            d_AB = jnp.sqrt(
+                (Rs[3 * A] - Rs[3 * B]) ** 2
+                + (Rs[3 * A + 1] - Rs[3 * B + 1]) ** 2
+                + (Rs[3 * A + 2] - Rs[3 * B + 2]) ** 2
+            )
+            e_nn += Zs[A] * Zs[B] / d_AB
+
+    return e_nn
 
 
-# gg_E_calc = grad(g_E_calc)
-# eval_E = gg_E_calc(1.0,2.0,3.0)
-# print(eval_E)
+def get_grad(coordinates, charges):
+    gradient = []
+    natoms = len(charges)
+    state = np.zeros(
+        2 + 3 * natoms, dtype=int
+    )  # makes an array that derv can work with
+    flat_coords = coordinates.reshape(3 * natoms)
+    for item in range(2, len(state)):
+        state[item] = 1  # change the array to the wanted derivation for the hessian
+        eval_E = derv(
+            E_NN,
+            [charges.size, charges, *flat_coords],
+            state,
+        )
+        gradient.append(eval_E)
+        state[item] = 0  # reset array
+
+    gradient = np.array(gradient)
+    gradient = gradient.reshape((natoms, 3))
+    return gradient
+
+
+# this will generate the full hessian
+def get_hessian(coordinates, charges):
+    natoms = len(charges)
+    state = np.zeros(2 + 3 * natoms, dtype=int)
+    hessian = np.zeros(
+        (natoms, 3) * 2
+    )  # reshaping the hessian gave errors so here's created a priori
+    # these will keep track whether we are on the x y or z coordinate
+    i_xyz = 0
+    a_xyz = 0
+    flat_coords = coordinates.reshape(3 * natoms)
+    for i in range(2, len(state)):
+        state[i] += 1  # change the item state
+        for a in range(2, len(state)):
+            state[a] += 1
+            eval_E = derv(
+                E_NN,
+                [charges.size, charges, *flat_coords],
+                state,
+            )
+            # print(f"mapping to {(i-2)//3},{i_xyz},{(a-2)//3},{a_xyz}") METADATA
+            hessian[(i - 2) // 3, i_xyz, (a - 2) // 3, a_xyz] = eval_E
+            state[a] -= 1  # reset
+            a_xyz += 1  # change x y z
+            a_xyz *= a_xyz != 3  # reset if more than 2 (z)
+
+        state[i] -= 1
+        i_xyz += 1
+        i_xyz *= i_xyz != 3
+
+    return hessian
+
+
+"""
+coordinates = np.array([[1.0, 2.0, 3.0], [2.0, 1.0, 3.0]])
+charges = np.array([1.0, 1.0])
+
+grad_E = get_hessian(coordinates,charges)
+print(grad_E)
+"""
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -2,49 +2,76 @@ import jax_test3 as jt3
 import pytest as pt
 
 import analytical
+import numpy as np
+
+np.set_printoptions(linewidth=120)
 
 
 @pt.mark.parametrize(
-    "x_a, y_a, z_a, x_b, y_b, z_b, c_a, c_b",
+    "coordinates, charges",
     [
-        (1.0, 2.0, 3.0, 0.0, 0.0, 0.0, 1.0, 1.0),
-        (0.0, 0.0, 0.0, 1.0, 2.0, 3.0, 1.0, 1.0),
-        (-1.0, -2.0, -3.0, 0.0, 0.0, 0.0, 1.0, 1.0),
-        (0.0, 0.0, 0.0, -1.0, -2.0, -3.0, 1.0, 1.0),
-        (1.0, 2.0, 3.0, 0.0, 0.0, 0.0, 5.0, 2.0),
-        (1.0, 2.0, 3.0, 0.0, 0.0, 0.0, -1.0, -1.0),
-        (1.0, 2.0, 3.0, 0.0, 0.0, 0.0, -2.0, -3.0),
-        (1.0, 2.0, 3.0, 0.0, 0.0, 0.0, 1.0, -1.0),
-        (3.0, 2.0, 1.0, 1.0, 2.0, 3.0, 1.0, 1.0),
-        (1.0, 2.0, 3.0, 0.0, 0.0, 0.0, 0.0, 1.0),
-        (1.0, 2.0, 3.0, 0.0, 0.0, 0.0, 1.0, 0.0),
-        (1.0, 2.0, 3.0, 0.0, 0.0, 0.0, 0.0, 0.0),
+        (np.array([[1.0, 2.0, 3.0], [2.0, 1.0, 3.0]]), np.array([1.0, 2.0])),
+        (
+            np.array([[1.0, 2.0, 3.0], [2.0, 1.0, 3.0], [3.0, 1.0, 2.0]]),
+            np.array([2.0, 1.0, 3.0]),
+        ),
+        (
+            np.array(
+                [[1.0, 2.0, 3.0], [2.0, 1.0, 3.0], [3.0, 1.0, 2.0], [5.0, 4.0, 3.0]]
+            ),
+            np.array([3.0, 4.0, 1.0, 7.0]),
+        ),
+        (
+            np.array(
+                [
+                    [1.0, 2.0, 3.0],
+                    [2.0, 1.0, 3.0],
+                    [3.0, 1.0, 2.0],
+                    [5.0, 4.0, 3.0],
+                    [4.0, 3.0, 5.0],
+                ]
+            ),
+            np.array([7.0, 6.0, 1.0, 5.0, 5.0]),
+        ),
     ],
 )
-def test_grad(x_a, y_a, z_a, x_b, y_b, z_b, c_a, c_b):
-    ref_grad = analytical.gradient(x_a, y_a, z_a, x_b, y_b, z_b, c_a, c_b)
-    jax_grad = jt3.get_grad(6, x_a, y_a, z_a, x_b, y_b, z_b, c_a, c_b)
+def test_grad(coordinates, charges):
+    ref_grad = analytical.gradient(coordinates, charges)
+    jax_grad = jt3.get_grad(coordinates, charges)
     assert jax_grad == pt.approx(ref_grad)
 
 
 @pt.mark.parametrize(
-    "x_a, y_a, z_a, x_b, y_b, z_b, c_a, c_b",
+    "coordinates, charges",
     [
-        (1.0, 2.0, 3.0, 0.0, 0.0, 0.0, 1.0, 1.0),
-        (0.0, 0.0, 0.0, 1.0, 2.0, 3.0, 1.0, 1.0),
-        (-1.0, -2.0, -3.0, 0.0, 0.0, 0.0, 1.0, 1.0),
-        (0.0, 0.0, 0.0, -1.0, -2.0, -3.0, 1.0, 1.0),
-        (1.0, 2.0, 3.0, 0.0, 0.0, 0.0, 5.0, 2.0),
-        (1.0, 2.0, 3.0, 0.0, 0.0, 0.0, -1.0, -1.0),
-        (1.0, 2.0, 3.0, 0.0, 0.0, 0.0, -2.0, -3.0),
-        (1.0, 2.0, 3.0, 0.0, 0.0, 0.0, 1.0, -1.0),
-        (3.0, 2.0, 1.0, 1.0, 2.0, 3.0, 1.0, 1.0),
-        (1.0, 2.0, 3.0, 0.0, 0.0, 0.0, 0.0, 1.0),
-        (1.0, 2.0, 3.0, 0.0, 0.0, 0.0, 1.0, 0.0),
-        (1.0, 2.0, 3.0, 0.0, 0.0, 0.0, 0.0, 0.0),
+        (np.array([[1.0, 2.0, 3.0], [2.0, 1.0, 3.0]]), np.array([1.0, 2.0])),
+        (
+            np.array([[1.0, 2.0, 3.0], [2.0, 1.0, 3.0], [3.0, 1.0, 2.0]]),
+            np.array([2.0, 1.0, 3.0]),
+        ),
+        (
+            np.array(
+                [[1.0, 2.0, 3.0], [2.0, 1.0, 3.0], [3.0, 1.0, 2.0], [5.0, 4.0, 3.0]]
+            ),
+            np.array([3.0, 4.0, 1.0, 7.0]),
+        ),
+        (
+            np.array(
+                [
+                    [1.0, 2.0, 3.0],
+                    [2.0, 1.0, 3.0],
+                    [3.0, 1.0, 2.0],
+                    [5.0, 4.0, 3.0],
+                    [4.0, 3.0, 5.0],
+                ]
+            ),
+            np.array([7.0, 6.0, 1.0, 5.0, 5.0]),
+        ),
     ],
 )
-def test_hessian(x_a, y_a, z_a, x_b, y_b, z_b, c_a, c_b):
-    ref_hess = analytical.hessian(x_a, y_a, z_a, x_b, y_b, z_b, c_a, c_b)
-    jax_hess = jt3.get_hessian(6, x_a, y_a, z_a, x_b, y_b, z_b, c_a, c_b)
+def test_hessian(coordinates, charges):
+    ref_hess = analytical.hessian_redundant(coordinates, charges)
+    print(f"ref_hess\n{ref_hess}")
+    jax_hess = jt3.get_hessian(coordinates, charges)
+    print(f"jax_hess\n{jax_hess}")
     assert jax_hess == pt.approx(ref_hess)
